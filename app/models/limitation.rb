e49aeda606b5826ab751577ab7c7df95adbe6ba8
Nils Bartels <nils@nils.bartels.xyz>
2023-12-21 10:57:30 +0100

    limitations should be creatable in the past

--- app/models/limitation.rb
+++ app/models/limitation.rb
@@ -8,7 +8,6 @@ class Limitation < ApplicationRecord
   validates :name, uniqueness: true
   validates :name, length: { in: 2..50 }
 
-  validates :start_date, comparison: { greater_than: DateTime.now.utc }
   validates :end_date, comparison: { greater_than: :start_date }
 
   belongs_to :limitable, polymorphic: true, optional: true
